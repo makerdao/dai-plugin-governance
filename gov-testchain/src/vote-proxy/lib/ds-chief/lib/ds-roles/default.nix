af8d4e111125d7362999fa2ddaab221fe0f11bb0
Phil Bain <bain.pmcg@gmail.com>
2018-10-17 17:31:48 +0200

    fix submodule issue with contracts

--- gov-testchain/src/vote-proxy/lib/ds-chief/lib/ds-roles/default.nix
+++
@@ -1,5 +0,0 @@
-{ solidityPackage, dappsys }: solidityPackage {
-  name = "ds-roles";
-  deps = with dappsys; [ds-auth ds-test];
-  src = ./src;
-}
--- gov-testchain/src/vote-proxy/lib/ds-chief/lib/ds-thing/default.nix
+++
@@ -1,5 +0,0 @@
-{ solidityPackage, dappsys }: solidityPackage {
-  name = "ds-thing";
-  deps = with dappsys; [ds-auth ds-note ds-math ds-test];
-  src = ./src;
-}
