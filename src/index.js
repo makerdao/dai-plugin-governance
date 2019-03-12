5b0bf14bcf36396a2349b3a29c3d7e30d0d51581
Lawrence Wang <l@lw.io>
2019-03-12 12:12:51 -0700

    toggle kovan addresses based on mcd option
    always override GOV token address

--- src/index.js
+++ src/index.js
@@ -1,40 +1,41 @@
 import { map, prop } from 'ramda';
-import { createCurrency } from '@makerdao/currency';
-import { VOTE_PROXY_FACTORY, MCD_ADM, POLLING } from './utils/constants';
+import { VOTE_PROXY_FACTORY, MCD_ADM, MKR, POLLING } from './utils/constants';
 import ChiefService from './ChiefService';
 import PollingService from './PollingService';
 import VoteProxyService from './VoteProxyService';
 import VoteProxyFactoryService from './VoteProxyFactoryService';
 
-const contractAddresses = {
-  kovan: require('../contracts/addresses/kovan.json'),
-  mainnet: require('../contracts/addresses/mainnet.json')
-};
-
-try {
-  const testnetAddresses = require('../contracts/addresses/testnet.json');
-  contractAddresses.testnet = testnetAddresses;
-} catch (err) {
-  // do nothing here; throw an error only if we later attempt to use ganache
-}
-
-const addContracts = {
-  [MCD_ADM]: {
-    address: map(prop('MCD_ADM'), contractAddresses),
-    abi: require('../contracts/abis/DSChief.json')
-  },
-  [VOTE_PROXY_FACTORY]: {
-    address: map(prop('VOTE_PROXY_FACTORY'), contractAddresses),
-    abi: require('../contracts/abis/VoteProxyFactory.json')
-  },
-  [POLLING]: {
-    address: map(prop('POLLING'), contractAddresses),
-    abi: require('../contracts/abis/Polling.json')
-  }
-};
-
 export default {
-  addConfig: function(config, { network = 'mainnet' }) {
+  addConfig: function(config, { network = 'mainnet', mcd }) {
+    const contractAddresses = {
+      kovan: mcd
+        ? require('../contracts/addresses/kovan-mcd.json')
+        : require('../contracts/addresses/kovan.json'),
+      mainnet: require('../contracts/addresses/mainnet.json')
+    };
+
+    try {
+      contractAddresses.testnet = require('../contracts/addresses/testnet.json');
+    } catch (err) {
+      // do nothing here; throw an error only if we later attempt to use ganache
+    }
+
+    const addContracts = {
+      [MCD_ADM]: {
+        address: map(prop('MCD_ADM'), contractAddresses),
+        // TODO check for MCD-specific version of DSChief
+        abi: require('../contracts/abis/DSChief.json')
+      },
+      [VOTE_PROXY_FACTORY]: {
+        address: map(prop('VOTE_PROXY_FACTORY'), contractAddresses),
+        abi: require('../contracts/abis/VoteProxyFactory.json')
+      },
+      [POLLING]: {
+        address: map(prop('POLLING'), contractAddresses),
+        abi: require('../contracts/abis/Polling.json')
+      }
+    };
+
     let makerConfig = {
       ...config,
       additionalServices: ['chief', 'polling', 'voteProxy', 'voteProxyFactory'],
@@ -42,21 +43,17 @@ export default {
       polling: [PollingService],
       voteProxy: [VoteProxyService],
       voteProxyFactory: [VoteProxyFactoryService],
-      smartContract: { addContracts }
-    };
-
-    if (network === 'kovan') {
-      const MKR = createCurrency('MKR');
-      makerConfig.token = {
+      smartContract: { addContracts },
+      token: {
         erc20: [
           {
             currency: MKR,
             symbol: MKR.symbol,
-            address: contractAddresses.kovan.MCD_GOV
+            address: contractAddresses[network].MCD_GOV
           }
         ]
-      };
-    }
+      }
+    };
 
     return makerConfig;
   }
