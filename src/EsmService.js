f533a63677c8c9802e0801f2c7313cb07bfb2bd5
Tyler Sorensen <tyler@makerdao.com>
2019-10-03 16:41:06 +0100

    add allowance check to esm.join

--- src/EsmService.js
+++ src/EsmService.js
@@ -4,17 +4,22 @@ import { getCurrency } from './utils/helpers';
 
 export default class EsmService extends PrivateService {
   constructor(name = 'esm') {
-    super(name, ['smartContract', 'web3']);
+    super(name, ['smartContract', 'web3', 'allowance']);
   }
 
   // Writes -----------------------------------------------
 
-  join(amt, unit = MKR) {
+  async join(amt, unit = MKR) {
+    await this.get('allowance').requireAllowance(MKR, this._esmAddress());
     const mkrAmt = getCurrency(amt, unit).toFixed('wei');
     return this._esmContract().join(mkrAmt);
   }
 
   _esmContract() {
     return this.get('smartContract').getContractByName(ESM);
+  }
+
+  _esmAddress() {
+    return this.get('smartContract').getContractAddressByName(ESM);
   }
 }
