48b41b38a856c9246274683c37ac436cd3258a23
Ethan <ebennett19@gmail.com>
2019-12-17 16:25:25 -0500

    Add testchain outputs installation script

---
+++ scripts/install-testchain-outputs.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -e
+
+CWD=`dirname $0`
+CONTRACTS=$CWD/../contracts
+SOURCE=${1:-$CWD/../../../node_modules/@makerdao/testchain}
+
+CHIEF=`jq ".OLD_CHIEF" "$SOURCE/out/addresses.json"`
+jq ".CHIEF=$CHIEF" $CONTRACTS/addresses/testnet.json > testnet.tmp && mv testnet.tmp $CONTRACTS/addresses/testnet.json
+cp $SOURCE/out/DSChief.abi $CONTRACTS/abis/DSChief.json
+
+IOU=`jq ".OLD_IOU" "$SOURCE/out/addresses.json"`
+jq ".IOU=$IOU" $CONTRACTS/addresses/testnet.json > testnet.tmp && mv testnet.tmp $CONTRACTS/addresses/testnet.json
+
+VOTE_PROXY_FACTORY=`jq ".OLD_VOTE_PROXY_FACTORY" "$SOURCE/out/addresses.json"`
+jq ".VOTE_PROXY_FACTORY=$VOTE_PROXY_FACTORY" $CONTRACTS/addresses/testnet.json > testnet.tmp && mv testnet.tmp $CONTRACTS/addresses/testnet.json
+cp $SOURCE/out/VoteProxyFactory.abi $CONTRACTS/abis/VoteProxyFactory.json
+
+POLLING=`jq ".POLLING" "$SOURCE/out/addresses.json"`
+jq ".POLLING=$POLLING" $CONTRACTS/addresses/testnet.json > testnet.tmp && mv testnet.tmp $CONTRACTS/addresses/testnet.json
+cp $SOURCE/out/Polling.abi $CONTRACTS/abis/Polling.json
+
+GOV=`jq ".GOV" "$SOURCE/out/addresses.json"`
+jq ".GOV=$GOV" $CONTRACTS/addresses/testnet.json > testnet.tmp && mv testnet.tmp $CONTRACTS/addresses/testnet.json
+
+# Relevant contracts from MCD:
+for CONTRACT in "MCD_ESM","ESM" "MCD_END","End"
+do
+  IFS=',' read NAME ABI <<< "${CONTRACT}"
+  ADDRESS=`jq ".$NAME" "$SOURCE/out/addresses-mcd.json"`
+  KOVAN_ADDRESS=`jq ".$NAME" "$CONTRACTS/../../dai-plugin-mcd/contracts/addresses/kovan.json"`
+  SUFFIX="_1"
+  jq ".$NAME$SUFFIX=$ADDRESS" $CONTRACTS/addresses/testnet.json > testnet.tmp && mv testnet.tmp $CONTRACTS/addresses/testnet.json
+  jq ".$NAME$SUFFIX=$KOVAN_ADDRESS" $CONTRACTS/addresses/kovan.json > testnet.tmp && mv testnet.tmp $CONTRACTS/addresses/kovan.json
+  cp $SOURCE/out/mcd/$ABI.abi $CONTRACTS/abis/$ABI.json
+done
